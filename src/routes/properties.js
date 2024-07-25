9ff8c6fbf8403bda2f4010b0c33dc5bab7306cff
AntonioValentinSayago <cesar_anto30@hotmail.com>
2024-07-24 20:48:30 -0600

    [feat] : First method of the properties.js file

---
+++ src/routes/properties.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router();
+
+//** Definicion del Metodo GET : properties */
+/**
+* @swagger
+ * /properties:
+ *   get:
+ *     summary: Get all properties
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   address:
+ *                     type: string
+ *                     example: "123 Main St, Anytown, USA"
+ *                   price:
+ *                     type: number
+ *                     example: 250000.00
+ *                   bedrooms:
+ *                     type: integer
+ *                     example: 3
+ *                   bathrooms:
+ *                     type: integer
+ *                     example: 2
+ *                   listing_date:
+ *                     type: string
+ *                     format: date
+ *                     example: "2023-01-15"
+ */
